a0e55ee26ac77fc62c5f8272c693872d16f1b7a6
leexa <lee.x.a90@gmail.com>
2018-06-05 11:19:26 +0800

    done

--- analysis.sh
+++
@@ -1,7 +0,0 @@
-echo 'Ensure you are in anaconda environment'
-source deactivate
-source activate stapled_peptide
-cd data
-python 1_get_smiles.py
-python 2_get_RdkitFeatures.py
-python 3_analysis.py
